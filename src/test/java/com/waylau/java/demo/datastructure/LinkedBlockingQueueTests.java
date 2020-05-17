1cfa5347f808e05c814561f6243bb3bdac8881d0
waylau <waylau521@gmail.com>
2020-05-17 11:26:16 +0800

    test

---
+++ src/test/java/com/waylau/java/demo/datastructure/LinkedBlockingQueueTests.java
@@ -0,0 +1,24 @@
+/**
+ * Welcome to https://waylau.com
+ */
+package com.waylau.java.demo.datastructure;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * LinkedBlockingQueue Test
+ * 
+ * @since 1.0.0 2020年5月9日
+ * @author <a href="https://waylau.com">Way Lau</a>
+ */
+class LinkedBlockingQueueTests {
+
+	/**
+	 * 
+	 */
+	public LinkedBlockingQueueTests() {
+		new LinkedBlockingQueue();
+	}
+	
+
+}
